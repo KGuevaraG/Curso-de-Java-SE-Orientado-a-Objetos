4fd083e20e2043863995f84292cf8db1bcb76f67
Kevin Guevara <kevinernestgg@outlook.es>
2023-04-22 12:39:08 -0600

    Clases Anidadas

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
      static int id = 0;
-     String name;
-     String email;
-     String speciality;
+     private String name;
+     private String email;
+     private String speciality;
+     private ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+     public void addAvailabeAppointment(Date date, String time){
+         availableAppointments.add(new AvailableAppointment(date,time));
+     }
+     public ArrayList<AvailableAppointment> getAvailableAppointments(){
+         return availableAppointments;
+     }
      Doctor(){
          System.out.println("Construyendo el objeto doctor");
          id++;
@@ -18,5 +28,31 @@ public class Doctor {
     }
     public void showId(){
          System.out.println("Id Doctor: " + id);
+    }
+    public static class AvailableAppointment{
+        private int id_availableApointment;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,12 +1,22 @@
+import java.util.Date;
+
 import static UI.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
         //showMenu();
         Doctor myDoctor = new Doctor("Juan Vazques","Pediatria");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+
         Patient patient = new Patient("Alejandra","alejandra@mail.com");
+
+        myDoctor.addAvailabeAppointment(new Date(), "4pm");
+        myDoctor.addAvailabeAppointment(new Date(), "10am");
+        myDoctor.addAvailabeAppointment(new Date(), "1pm");
+
+        System.out.println(myDoctor.getAvailableAppointments());
+        for (Doctor.AvailableAppointment aA:myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " "+aA.getTime());
+        }
     }
 }
 
