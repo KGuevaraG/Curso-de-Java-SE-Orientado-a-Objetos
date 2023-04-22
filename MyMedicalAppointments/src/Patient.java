3854c03b6b8f8c7f14b9ce73f03e20b50e7a9e79
Kevin Guevara <kevinernestgg@outlook.es>
2023-04-22 11:55:33 -0600

    Getters y Setters

--- MyMedicalAppointments/src/Patient.java
+++ MyMedicalAppointments/src/Patient.java
@@ -4,6 +4,7 @@ public class Patient {
     private String name;
     private String email;
     private String address;
+    private String phoneNumber;
     private String birhtday;
     private double weight;
     private double height;
@@ -12,5 +13,81 @@ public class Patient {
     Patient(String name, String email){
         this.name=name;
         this.email=email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+    public String getWeight(){
+        return this.weight + "Kg.";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length()>8){
+            System.out.println("El numero telefonico debe ser de 8 digitos maximo");
+        } else if (phoneNumber.length()==8) {
+            this.phoneNumber = phoneNumber;
+        }
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirhtday() {
+        return birhtday;
+    }
+
+    public void setBirhtday(String birhtday) {
+        this.birhtday = birhtday;
+    }
+
+    public String getHeight() {
+        return height + "Mts.";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
     }
 }
