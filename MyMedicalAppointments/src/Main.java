a8d880f2bc44259218ff874025aeb394ce079bf9
Kevin Guevara <kevinernestgg@outlook.es>
2023-04-22 11:07:41 -0600

    Final: Variables Constantes

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -2,12 +2,6 @@ import static UI.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name="Alejandro Rodriguez";
-        myDoctor.showName();
-        myDoctor.showId();
-        Doctor myDoctorK = new Doctor();
-        System.out.println(myDoctorK.id);
         showMenu();
     }
 }
