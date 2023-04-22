f3972e24df10ad606bd5e9a7940f42109fb431f3
Kevin Guevara <kevinernestgg@outlook.es>
2023-04-22 10:52:03 -0600

    Creando elementos estáticos

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,9 +1,10 @@
 public class Doctor {
-     int id;
+     static int id = 0;
      String name;
      String speciality;
      Doctor(){
          System.out.println("Construyendo el objeto doctor");
+         id++;
      }
      Doctor(String name){
          System.out.println("El nomre del doctor asirgano es "+name);
@@ -12,5 +13,8 @@ public class Doctor {
 
     public void showName(){
         System.out.println(name);
+    }
+    public void showId(){
+         System.out.println("Id Doctor: " + id);
     }
 }
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,7 +1,14 @@
+import static UI.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("AleJandro");
+        Doctor myDoctor = new Doctor();
         myDoctor.name="Alejandro Rodriguez";
         myDoctor.showName();
+        myDoctor.showId();
+        Doctor myDoctorK = new Doctor();
+        System.out.println(myDoctorK.id);
+        showMenu();
     }
 }
+
