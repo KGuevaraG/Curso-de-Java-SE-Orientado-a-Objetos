c0853592881e75a3204a4843deb7a4beeb9e3430
Kevin Guevara <kevinernestgg@outlook.es>
2023-04-21 23:21:14 -0600

    Método constructor

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -2,7 +2,12 @@ public class Doctor {
      int id;
      String name;
      String speciality;
-
+     Doctor(){
+         System.out.println("Construyendo el objeto doctor");
+     }
+     Doctor(String name){
+         System.out.println("El nomre del doctor asirgano es "+name);
+     }
      //Comportamientos
 
     public void showName(){
