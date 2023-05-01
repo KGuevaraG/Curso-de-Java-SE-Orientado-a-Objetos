a87509c9a331492e9f6a841356d6fbec32776804
Kevin Guevara <kevinernestgg@outlook.es>
2023-05-01 17:06:07 -0600

    Polimorfismo

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -27,9 +27,11 @@ public class Doctor extends User {
          return availableAppointments;
      }
 
+    @Override
+    public String toString() {
+        return super.toString()+"\nSpeciality: "+speciality+"\nAvailable: "+availableAppointments.toString();
 
-     //Comportamientos
-
+    }
 
     public static class AvailableAppointment{
         private int id_availableApointment;
@@ -55,6 +57,11 @@ public class Doctor extends User {
 
         public void setTime(String time) {
             this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return "Available Appointments \nDate: "+date+"\nTime: "+time;
         }
     }
 }
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -5,18 +5,20 @@ import static UI.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
         //showMenu();
-        Doctor myDoctor = new Doctor("Juan Vazques","Pediatria");
+        Doctor myDoctor = new Doctor("Juan Vazques","juan@gmail.com+");
 
         Patient patient = new Patient("Alejandra","alejandra@mail.com");
 
         myDoctor.addAvailabeAppointment(new Date(), "4pm");
         myDoctor.addAvailabeAppointment(new Date(), "10am");
         myDoctor.addAvailabeAppointment(new Date(), "1pm");
-
+/*
         System.out.println(myDoctor.getAvailableAppointments());
         for (Doctor.AvailableAppointment aA:myDoctor.getAvailableAppointments()) {
             System.out.println(aA.getDate() + " "+aA.getTime());
         }
+*/
+        System.out.println(myDoctor);
     }
 }
 
